2aaeba4751c9f3d739d2a774746d70a88d057ab2
陈卓 <1486853830@qq.com>
2025-05-01 02:20:08 +0800

    结构更新

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,10 +2,14 @@ import { HuoQubing } from '../霍去病/huoqubing.js';
 import { LiuBang } from '../刘邦/liubang.js';
 import { XiangYu } from '../项羽/xiangyu.js';
 import { CaoCao } from '../曹操/caocao.js';
-import { QiuChuji} from '../丘处机/qiuchuji.js';
+import { QiuChuji } from '../丘处机/qiuchuji.js';
+import { KongZi } from '../孔子/kongzi.js';
+import { ZhangLiang } from '../张良/zhangliang.js';
+import { FanKuai } from '../樊哙/fankuai.js';
+import { ZhaoYun } from '../赵云/zhaoyun.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const path = window.location.pathname;
+    const path = window.location.pathname.toLowerCase();
     
     if (path.includes('huoqubing')) {
         const huoqubing = new HuoQubing();
@@ -21,6 +25,18 @@ document.addEventListener('DOMContentLoaded', () => {
         caocao.init();
     } else if (path.includes('qiuchuji')) {
         const qiuchuji = new QiuChuji();
-        qiuchuji.init(); 
+        qiuchuji.init();
+    } else if (path.includes('kongzi')) {
+        const kongzi = new KongZi();
+        kongzi.init();
+    } else if (path.includes('zhangliang')) {
+        const zhangliang = new ZhangLiang();
+        zhangliang.init();
+    } else if (path.includes('fankuai')) {
+        const fankuai = new FanKuai();
+        fankuai.init();
+    } else if (path.includes('zhaoyun')) {
+        const zhaoyun = new ZhaoYun();
+        zhaoyun.init();
     }
 });
