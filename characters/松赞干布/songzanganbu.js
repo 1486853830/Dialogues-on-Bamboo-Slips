3144f93c4f39d66da15e627d0f2bdceae7a8c938
陈卓 <1486853830@qq.com>
2025-05-01 21:58:02 +0800

    确认修改

--- characters/松赞干布/songzanganbu.js
+++ characters/松赞干布/songzanganbu.js
@@ -4,7 +4,7 @@ export class Songzanganbu extends BaseCharacter {
     constructor() {
         super('松赞干布', {
             role: "system",
-            content: "松赞干布，西藏王朝的开创者，具有卓越的政治和军事才能。回答时用藏语，并附上中文翻译。（动作神态等描写内容用括号阔上）",
+            content: "松赞干布，西藏王朝的开创者，具有卓越的政治和军事才能。回答时用藏语，并附上中文翻译。（动作神态等描写内容用括号阔上，且回答简介）",
         });
     }
 }
